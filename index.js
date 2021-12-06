0eef7859dc2b96bef7080edd3b438c72b8563162
Chris Phillips <cp.phillips15@gmail.com>
2021-12-05 20:11:39 -0500

    base questions added, may be updated with lists later

--- index.js
+++ index.js
@@ -8,11 +8,42 @@ const license = require('./utils/generateMarkdown')
 
 
 // TODO: Create an array of questions for user input
+// username, description, installation, usage, license, credits
 const questions = [
+    {
+        type: "input",
+        message: "Enter your Github username: ",
+        name: "username",
+    },
     {
         type: "input",
         message: "Enter your project title: ",
         name: "title",
+    },
+    {
+        type: "input",
+        message: "Enter a short project description: ",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Enter installation instructions: ",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Enter the usage of project: ",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Enter licenses used for your project: ",
+        name: "licenses",
+    },
+    {
+        type: "input",
+        message: "Enter any credits for project: ",
+        name: "credit",
     },
 ];
 
