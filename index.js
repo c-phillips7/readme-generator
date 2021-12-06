d15f1caa78d79c5b4d9c1355f5772c6d0fc8a24f
Chris Phillips <cp.phillips15@gmail.com>
2021-12-05 20:02:56 -0500

    required npm packages and did first test to ensure inquirer functioning

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 // TODO: Include packages needed for this application
 // add and require inquirer and fs
+const inquirer = require('inquirer');
+const fs = require('fs');
+const license = require('./utils/generateMarkdown')
+
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Enter your project title: ",
+        name: "title",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    await inquirer
+        .prompt(questions)
+        .then(answers => {
+            console.log(answers);
+        })
+    
+
+
+}
 
 // Function call to initialize app
 init();
