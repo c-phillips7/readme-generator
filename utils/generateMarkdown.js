e3caa3b89de5940caf7f12fced1f6a05b84566bb
Chris Phillips <cp.phillips15@gmail.com>
2021-12-05 20:35:46 -0500

    added readme string based on templates found online

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,41 @@
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
   ${data.desctiption}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${data.license}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  For any questions, contact me at ${data.email}.
 
 `;
 }
